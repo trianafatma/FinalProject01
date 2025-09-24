25a55f6769ec9c51c13300424e21cc801cde4c30
Triana Fatmawati <triana.f@gmail.com>
2025-09-24 20:48:56 +0700

    Commit Test1.java by trianafatmagit

--- Test1.java
+++ Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hello world 1");
+        System.out.println("Ditambahkan oleh user 2");
     }
     
 }
